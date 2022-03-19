a2d12822aac59b4864ec8123a5d6b94774b85ce7
Jose viejo <dviejo@kungfusoftware.es>
2022-03-19 11:35:14 +0100

    creacion de componentes

---
+++ src/componentes/Facturas.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Facturas() {
+  return (
+    <div>Facturas</div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={ <Home></Home>} />
+      <Route path="/facturas" element={ <Facturas></Facturas>} />
+      <Route path="/clientes" element={ <Clientes></Clientes>} />
+      <Route path="/productos" element={ <Productos></Productos>} />
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 )
