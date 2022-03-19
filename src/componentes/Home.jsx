beab7ba54b429e5a8dbb904b59e96a58fd99614d
Jose viejo <dviejo@kungfusoftware.es>
2022-03-19 11:56:06 +0100

    creacion de componentes

--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import {Link, Outlet} from 'react-router-dom'
 
 export default function Home() {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <div className="d-flex flex-column container fullscreen ">
+            <header>
+                <ul>
+                    <li><Link to="/clientes">Clientes</Link></li>
+                    <li><Link to="/productos">Productos</Link></li>
+                    <li><Link to="/facturas">Facturas</Link></li>
+                </ul>
+            </header>
+            <main className="flex-grow-1">
+                <Outlet />
+            </main>
+            <footer>pie</footer>
+        </div>
+    )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './componentes/Home'
+import Clientes from './componentes/Clientes'
+import Productos from './componentes/Productos'
+import Facturas from './componentes/Facturas'
 
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={ <Home></Home>} />
-      <Route path="/facturas" element={ <Facturas></Facturas>} />
-      <Route path="/clientes" element={ <Clientes></Clientes>} />
-      <Route path="/productos" element={ <Productos></Productos>} />
+      <Route path="/" element={<Home></Home>} >
+        <Route path="/facturas" element={<Facturas></Facturas>} />
+        <Route path="/clientes" element={<Clientes></Clientes>} />
+        <Route path="/productos" element={<Productos></Productos>} />
+      </Route>
     </Routes>
   </BrowserRouter>,
   document.getElementById('root')
